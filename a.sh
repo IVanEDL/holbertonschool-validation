a5525b7ddfa83f2fb42772c882d6f51086b19a6e
IVanEDL <ivanaq3d@gmail.com>
2023-05-14 20:19:38 -0300

    Add coso

--- a.sh
+++ a.sh
@@ -1,5 +1,2 @@
 #!/bin/bash
-# binary will be $(go env GOPATH)/bin/golangci-lint
-curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b $(go env GOPATH)/bin 
-
-golangci-lint --version
+go install github.com/golangci/golangci-lint/cmd/golangci-lint@
