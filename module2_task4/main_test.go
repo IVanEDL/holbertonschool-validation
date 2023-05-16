e5d1163f717f25d8380946a562a51d8d9c96522c
root <IVanEDL>
2023-05-16 11:34:32 -0700

    Task 4 fix

--- module2_task4/main_test.go
+++ module2_task4/main_test.go
@@ -30,7 +30,18 @@ func Test_HelloHandler(t *testing.T) {
       responseCode: 200,
       body:         "Hello Rosalind Franklin!",
     },
-    // INSERT MORE TESTS HERE
+    {
+      name:         "Name pero namen't",
+      queryString:  "name=",
+      responseCode: 400,
+      body:         "",
+    },
+    {
+      name:         "Namen't",
+      queryString:  "",
+      responseCode: 200,
+      body:         "Hello there!",
+    },
   }
   for _, tt := range tests {
     t.Run(tt.name, func(t *testing.T) {
